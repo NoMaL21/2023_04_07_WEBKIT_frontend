7ab649e8e4f6a663eafcc74d9a33b7cabe5b63be
NoMaL <dntjd8509@gmail.com>
2023-03-30 16:15:57 +0900

    7. React로 안녕하세요! 출력하기
    커밋

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Hello extends React.Component{
+  render(){
+    return(
+      <div className="Hello">
+        <label for="hello">Hello World?!</label>
+      </div>
+    )
+  }
+}
+
+export default Hello;
