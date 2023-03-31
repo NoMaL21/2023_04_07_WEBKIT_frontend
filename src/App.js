a22f60dda2a83ec9bb734e402e94da8a4189242c
NoMaL <dntjd8509@gmail.com>
2023-03-31 14:45:55 +0900

    11. React 로 목록 추가,수정,삭제하기
    커밋

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Todo from "./Todo";
-import AddTodo from './AddTodo'
-import {Paper, List, Container}from "@material-ui/core";
+import AddTodo from "./AddTodo";
+import { Paper, List, Container } from "@material-ui/core";
 import "./App.css";
 
 class App extends React.Component {
@@ -9,28 +9,36 @@ class App extends React.Component {
         super(props);
         this.state = {
             items: [
-                { id: 0, title: "Todo 1", done:true},
-                { id: 1, title: "Todo 2", done:false},
+                { id: "todo0", title: "Todo 1", done: true },
+                { id: "todo1", title: "Todo 2", done: false },
             ],
         };
     }
 
-    add =(item)=>{
+    add = (item) => {
         const thisItems = this.state.items;
-        item.id ="ID-" + this.Itemslength;
+        item.id = "ID-" + this.Itemslength;
         item.done = false;
         thisItems.push(item);
-        this.setState({items:thisItems});
-        console.log("items:",this.state.items);
+        this.setState({ items: thisItems });
+        console.log("items:", this.state.items);
+    };
+
+    //delete 함수 추가
+    delete =(item)=>{
+        const thisItems =this.state.items;
+        const newItems = thisItems.filter(e => e.id !== item.id); //id가 같은지 확인
+        this.setState({items:newItems},()=>{
+            console.log("Update Items : ", this.state.items)
+        });
     }
 
     render() {
-
-        var todoItems=this.state.items.length > 0 &&(
-            <Paper style={{margin:16}}>
+        var todoItems = this.state.items.length > 0 && (
+            <Paper style={{ margin: 16 }}>
                 <List>
-                    {this.state.items.map((item,ids)=>(
-                        <Todo item={item} key={item.id}/>
+                    {this.state.items.map((item, ids) => (
+                        <Todo item={item} key={item.id} delete={this.delete} />
                     ))}
                 </List>
             </Paper>
@@ -39,8 +47,8 @@ class App extends React.Component {
         return (
             <div className="App">
                 <Container maxWidth="md">
-                <AddTodo add={this.add}/>
-                <div className="TodoList">{todoItems}</div>
+                    <AddTodo add={this.add} />
+                    <div className="TodoList">{todoItems}</div>
                 </Container>
             </div>
         );
