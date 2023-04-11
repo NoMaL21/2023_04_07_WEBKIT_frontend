fb4dcf26d4c0fce94b9b53c16db2ab1fb298914c
NoMaL <dntjd8509@gmail.com>
2023-04-11 13:25:08 +0900

    페이징 구현됨

--- src/App.js
+++ src/App.js
@@ -2,107 +2,128 @@ import React from "react";
 import Todo from "./Todo";
 import AddTodo from "./AddTodo";
 import DeleteTodo from "./DeleteTodo";
-import { Paper, List, Container, Grid, Button, AppBar, Toolbar, Typography} from "@material-ui/core";
+import { Paper, List, Container, Grid, Button, AppBar, Toolbar, Typography } from "@material-ui/core";
 import "./App.css";
 import { call, signout } from "./service/ApiService";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: [],
-            loading:true,
-        };
-    }
-
-    add = (item) => {
-        call("/todo","POST",item).then((response) => 
-            this.setState({items:response.data})
-            );
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      loading: true,
+      page: 1, // 페이지 번호
+      itemsPerPage: 5 // 페이지당 아이템 수
     };
+  }
 
-    //delete 함수 추가
-    delete =(item)=>{
-        call("/todo","DELETE",item).then((response) => 
-            this.setState({items:response.data})
-            );
-    }
+  add = (item) => {
+    call("/todo", "POST", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
 
-    update = (item)=>{
-        call("/todo","PUT",item).then((response) => 
-        this.setState({items:response.data})
-        );
-    }
+  //delete 함수 추가
+  delete = (item) => {
+    call("/todo", "DELETE", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
 
-    deleteForCompleted = () => {
-        const thisItems = this.state.items;
-        console.log("Before deleteForCompleted Items : ", this.state.items);
-        thisItems.map((e) => {
-            if (e.done === true) {
-                call("/todo", "DELETE", e).then((response) => 
-                    this.setState({ items: response.data })
-            );
-            }
-        });
-    }
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
 
-    componentDidMount(){
-        call("/todo","GET",null).then((response) => 
-            this.setState({items:response.data, loading:false })
-        );
-    }
-
-    render() {
-        var todoItems = this.state.items.length > 0 && (
-            <Paper style={{ margin: 16 }}>
-                <List>
-                    {this.state.items.map((item, idx) => (
-                        <Todo item={item} key={item.id} delete={this.delete} update={this.update} />
-                    ))}
-                </List>
-            </Paper>
-        );
+  deleteForCompleted = () => {
+    const { items, page, itemsPerPage } = this.state;
+    console.log("Before deleteForCompleted Items : ", items);
     
-        var navigationBar = (
-            <AppBar position="static">
-                <Toolbar>
-                    <Grid justify="space-between" container>
-                        <Grid item>
-                            <Typography variant="h6">오늘의 할일</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Button color="inherit" onClick={signout}>logout
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </Toolbar>
-            </AppBar>
-        )
+    const itemsToDelete = items.filter((item) => item.done);
+    
+    Promise.all(
+      itemsToDelete.map((item) => call("/todo", "DELETE", item))
+    ).then((responses) => {
+      const newItems = items.filter((item) => !item.done);
+      const lastPage = Math.ceil(newItems.length / itemsPerPage);
+      this.setState({ items: newItems, page: lastPage });
+    });
+  };
 
+  componentDidMount() {
+    call("/todo", "GET", null).then((response) =>
+      this.setState({ items: response.data, loading: false })
+    );
+  }
 
-        var todoListPage = (
-            <div>
-                {navigationBar}
-                <Container maxWidth="md">
-                    <AddTodo add={this.add} />
-                    <div className="TodoList">{todoItems}</div>
-                    <DeleteTodo deleteForCompleted={this.deleteForCompleted}/>
-                </Container>
-            </div>
-        );
+  // 페이지 이동 버튼 클릭 시 호출되는 함수
+  handlePageChange = (page) => {
+    this.setState({ page });
+  };
 
-        var loadingPage = <h1>Loading...</h1>
-        var content = loadingPage;
+  render() {
+    const { items, page, itemsPerPage } = this.state;
 
-        if(!this.state.loading){
-            content = todoListPage;
-        }
+    // 페이지 계산
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const displayItems = items.slice(startIndex, endIndex);
 
-        return (
-            <div className="App">
-                {content}
-            </div>
-        );
+    // todo 아이템 생성
+    const todoItems = displayItems.length > 0 && (
+        <Paper style={{ margin: 16 }}>
+          <List>
+            {displayItems.map((item, idx) => (
+              <Todo item={item} key={item.id} delete={this.delete} update={this.update} />
+            ))}
+          </List>
+        </Paper>
+      );
+
+    // 페이지 이동 버튼 생성
+    const pageButtons = (
+        <div className="Pagination">
+          {Array.from({ length: Math.ceil(items.length / itemsPerPage) }, (_, i) => (
+            <Button key={i} onClick={() => this.handlePageChange(i + 1)}>
+              {i + 1}
+            </Button>
+          ))}
+        </div>
+      );
+
+    // 네비게이션 바 코드
+    const navigationBar = (
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Todo App
+          </Typography>
+          <Button color="inherit" onClick={signout}>
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+    );
+    
+    const noItemsFound = !todoItems && <h3>No items found.</h3>;
+
+    return (
+        <div>
+          {navigationBar}
+          <Container maxWidth="md">
+            <AddTodo add={this.add} />
+            {todoItems && (
+              <div>
+                <div className="TodoList">{todoItems}</div>
+                {pageButtons}
+              </div>
+            )}
+            {noItemsFound}
+            <DeleteTodo deleteForCompleted={this.deleteForCompleted} />
+          </Container>
+        </div>
+      );
     }
 }
 
